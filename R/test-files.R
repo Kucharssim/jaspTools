626567f41e2e2daeb8745eea921eff50d90de5e4
Kucharssim <kucharssim@gmail.com>
2022-03-07 17:20:02 +0100

    vignette testing

--- R/test-files.R
+++ R/test-files.R
@@ -18,12 +18,6 @@ useTest <- function(analysis, name=NULL, open = rlang::is_interactive()) {
   .useTest(analysis, name, "consistency", open)
 }
 
-#' @rdname useTest
-#' @export
-useVerifiedTest <- function(analysis, name=NULL, open = rlang::is_interactive()) {
-  .useTest(analysis, name, "verified", open)
-}
-
 .useTest <- function(analysis, name=NULL, type = c("consistency", "verified"), open = rlang::is_interactive()) {
   type <- match.arg(type)
 
@@ -40,7 +34,115 @@ useVerifiedTest <- function(analysis, name=NULL, open = rlang::is_interactive())
                        "consistency" = "consistency-test.R",
                        "verified"    = "verified-test.R"
                        )
-    usethis::use_template(template, save_as = path, open = FALSE, package = "jaspTools", data = list(analysis = analysis, name = name))
+    usethis::use_template(template = template, save_as = path, open = FALSE, package = "jaspTools", data = list(analysis = analysis, name = name))
   }
   usethis::edit_file(usethis::proj_path(path), open = open)
+}
+
+#' @title Create or edit JASP vignettes
+#'
+#' @description This function mimics \link[usethis]{usethis::use_article()},
+#' but uses a template for the vignette that is tailored for JASP style vignettes.
+#' This vignette then can be converted into a verified unit test.
+#'
+#' @details Running this function configures the module to have test vignettes.
+#' If necessary, it creates the \code{vignettes/tests} folder, adds \code{knitr} to the \code{Suggests}
+#' field and configures \code{VignetteBuilder} in the \code{DESCRIPTION} file,
+#' adds \code{inst/doc} folder in \code{.gitignore}, and adds \code{vignettes/tests} into \code{.Rbuildignore}.
+#'
+#' Then, it creates a file based on the analysis name and name of the test file.
+#'
+#' \link{testVignette()} and \link{testVignettes()} can be used for testing the vignette.
+#'
+#' @param analysis String name of the analysis that is shown in the vignette (case sensitive).
+#' @param name String name of the test file.
+#' @param title String title of the vignette (optional).
+#' @param open Do you want to open the file automatically in RStudio?
+#'
+#' @export
+useTestVignette <- function(analysis, name, title = name, open = rlang::is_interactive()) {
+  usethis:::use_dependency("knitr", "Suggests")
+  usethis:::use_description_field("VignetteBuilder", "knitr", overwrite = TRUE)
+  usethis::use_directory(file.path("vignettes", "tests", analysis))
+  usethis::use_build_ignore("vignettes/tests")
+  usethis::use_git_ignore("inst/doc")
+
+  title <- name
+  name <- tolower(gsub("[^a-zA-Z0-9_-]+", "-", name))
+  file <- paste0(analysis, "-", name, ".Rmd")
+  path <- file.path("vignettes", "tests", file)
+  if(!file.exists(path)) {
+    usethis::use_template(template = "vignette.Rmd", save_as = path, open = FALSE, package = "jaspTools",
+                          data = list(module   = usethis:::project_name(),
+                                      analysis = analysis,
+                                      name     = name,
+                                      title    = title)
+                          )
+  }
+  usethis::edit_file(usethis::proj_path(path), open = open)
+}
+
+
+#' @title Test JASP Vignettes
+#'
+#' @description These functions allow you to test vignettes in the \code{vignettes/tests} folder.
+#' \code{testVignette()} tests one specific file, \code{testVignettes()} can test all files in the folder,
+#' or files associated with a specific analysis.
+#'
+#' @details See \link{useTestVignette()} for help on how to create a JASP test vignette.
+#'
+#' @param name Name of the file inside of the \code{vignettes/tests} folder
+#' @param analysis String name of the analysis to be tested.
+#'
+#' @name testVignette
+NULL
+
+#' @rdname testVignette
+#' @export
+testVignette <- function(name) {
+  testFile <- .makeTestVignetteFile(name)
+  testthat::test_file(testFile)
+  file.remove(testFile)
+}
+
+#' @rdname testVignette
+#' @export
+testVignettes <- function(analysis) {
+  testFiles <- .makeTestVignetteFiles(analysis)
+  results <- testthat::test_local(filter = "vignette-")
+
+  for(file in testFiles) {
+    file.remove(file)
+  }
+  invisible(results)
+}
+
+.makeTestVignetteFile <- function(name) {
+  input <- usethis::proj_path("vignettes", "tests", name, ext = "Rmd")
+  if(!file.exists(input)) stop("Vignette does not exist!")
+
+  usethis::use_directory(file.path("tests", "testthat"))
+  file   <- paste0("test-vignette-", name, ".R")
+  output <- usethis::proj_path("tests", "testthat", file)
+
+  suppressMessages({
+    purl   <- knitr::purl(input = input, output = output, documentation = 1)
+  })
+  return(purl)
+}
+
+.makeTestVignetteFiles <- function(analysis) {
+  if(missing(analysis)) {
+    pattern <- NULL
+  } else {
+    pattern <- analysis
+  }
+
+  path <- usethis::proj_path("vignettes", "tests")
+  files <- list.files(path, recursive = FALSE, pattern = pattern, full.names = FALSE)
+  names <- gsub(".Rmd$", "", files)
+
+  testFiles <- vapply(names, .makeTestVignetteFile, character(1))
+
+  return(testFiles)
 }
